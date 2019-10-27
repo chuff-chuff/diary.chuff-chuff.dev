577fc5df231fb33adc52ef4d9a38e3bae5638bf0
chuff-chuff <norizaki85@gmail.com>
2019-10-27 13:07:12 +0000

    change settings to chuff-chuff's and add workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `chuff-chuff's diary`,
+    author: `chuff-chuff`,
+    description: `chuff-chuff's diary.`,
+    siteUrl: `https://diary.chuff-chuff.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `chuff_chuff09`,
     },
   },
   plugins: [
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `chuff-chuff's diary`,
+        short_name: `chuff-chuff's diary`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
